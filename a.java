15a57fb6c3001983449748070aaee5ea4f4d5593
Mutsuki Shimabukuro <e225733@ie.u-ryukyu.ac.jp>
2023-01-12 12:22:53 +0900

    完成形

---
+++ a.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class a {
+    public static void main(String[] args){
+        int guess;
+        Scanner stdin = new Scanner(System.in);
+        guess = stdin.nextInt();
+        
+    }
+}
--- jp/ac/uryukyu/ie/e225733/Suujiate.java
+++ jp/ac/uryukyu/ie/e225733/Suujiate.java
@@ -30,7 +30,23 @@ public class Suujiate{
     // コンピュータの答えを返すメソッド
     public static int getComAnswer() {
         // 作成した乱数を返す
-        return new Random().nextInt(1000)+100;//変更点++++++++
+
+        //ランダムで選ばれる数字で各桁の数字の重複を無くしている。
+        while(true){
+            //コンピューターの値は(１００〜９９９)から選ばれる。
+            int com_guess = new Random().nextInt(1000)+100;
+            //100の位
+            int com_place_100 = com_guess / 100;
+            //10の位
+            int com_place_10 = (com_guess - (com_place_100 * 100)) / 10;
+            //1の位
+            int com_place_1 = ((com_guess - (com_place_100 * 100)) - com_place_10 * 10);
+
+            if ((com_place_1 == com_place_10) || (com_place_1 == com_place_100) || (com_place_10 == com_place_100)){
+            } else {
+                return com_guess;
+            }
+        }
     }
     
     // キーボードから入力された数値(予想値)を返すメソッド
@@ -38,13 +54,23 @@ public class Suujiate{
         int guess;
         // キーボードの準備
         Scanner stdin = new Scanner(System.in);
-    
+
+        //break起こるまで、無限ループ
         while (true) {
             try {
                 System.out.println("予想値(１００～９９９）の入力");
                 // キーボードから予想値を読み込む
                 guess = stdin.nextInt();
-                break; // 整数として読み込めたので無限ループの終了
+                int len = String.valueOf(guess).length();
+
+                //２桁以外の数字で出力される
+                if(len!=3){
+                    System.out.println("３桁で入力してください");
+                //3桁の数字だとそのままループ抜ける
+                }else{
+                    break;// 整数として読み込めたので無限ループの終了
+                }
+            //数字以外が入力されたらエラーとして例外処理。
             } catch (Exception e) {
                 System.out.println("予想値は整数を入力してください");
                 stdin.next();
@@ -52,18 +78,21 @@ public class Suujiate{
         }
         return guess; // 読み込んだ整数を返す
     }
+
     
     // 答えと予想値が一致するかどうかを判定するメソッド
     public static boolean isJudgeAnswer(int com, int user) {
         int Eat = isJudgeEat(com, user);
         int Bite = isJudgeBite(com, user);
 
-        if ((Eat==3)&(Bite==3)) { // 3Eat,3Biteの時、正解・
+        // Eat=3,Bite=3の時、正解とする
+        if ((Eat==3)&(Bite==3)) {
             System.out.println("正解です！！");
             // 不正解なのでtrueを返す
             return true;
 
-        } else { // 不正解の場合
+        // 不正解の場合は、現在のEat数と、Bite数を返す。
+        } else {
             System.out.println(Eat+"イート,"+(Bite-Eat)+"バイト"+"です。");
             // 不正解なのでfalseを返す
             return false;
@@ -76,6 +105,7 @@ public class Suujiate{
         ArrayList<Integer> Eat_user = List_com(user);
         int Count_Eat = 0; 
 
+        //数字の位置が当たっているかを判断
         for(int i = 0; i< Eat_com.size();i++){
             int com_x = Eat_com.get(i);
             int user_x = Eat_user.get(i);
@@ -83,6 +113,7 @@ public class Suujiate{
                 Count_Eat++;
             }
         }
+        //Eatの数を返す
         return Count_Eat;
     }
 
@@ -92,6 +123,7 @@ public class Suujiate{
         ArrayList<Integer> Bite_user = List_com(user);
         int Count_bite = 0;
 
+        //数字が当たってるかを判断する
         for (int i:Bite_com){
             for (int j:Bite_user){
                 if(i==j){
@@ -99,32 +131,35 @@ public class Suujiate{
                 }
             }
         }
+        //Biteの数を返す
         return Count_bite;
     }
 
     //computerの数字の各桁をそれぞれリストに収納するメソッド
     public static ArrayList<Integer> List_com(int com){
         ArrayList<Integer> x = new ArrayList<>();
-    
         String com_String = Integer.toString(com);
+
+        //xに各桁の数字をリストとして保存
         for (int i = 0; i < com_String.length(); i++) {
             x.add(Character.getNumericValue(com_String.charAt(i)));
         }        
+
 
         return x;
     }
 
     //userの数字の各桁をそれぞれリストに収納するメソッド
     public static ArrayList<Integer> List_user(int user){
-        ArrayList<Integer> y = new ArrayList<>();
-    
+        ArrayList<Integer> y = new ArrayList<>();    
         String user_String = Integer.toString(user);
+
+        //yに各桁の数字をリストとして収納
         for (int i = 0; i < user_String.length(); i++) {
         y.add(Character.getNumericValue(user_String.charAt(i)));
         }        
 
         return y;
     }
-
 }
 
